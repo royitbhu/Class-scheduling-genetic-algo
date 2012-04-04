ed619e8e5ef6435ecdeea92e49453204212eb233
Rahul jain <rahuljain.roy@gmail.com>
2012-04-05 05:29:42 +0530

    Adding old Class scheduling project to git repo

---
+++ Genetic classes of class scheduling/tablepanel.java
@@ -0,0 +1,71 @@
+package schedule;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.EventQueue;
+import java.awt.event.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.Graphics2D;
+import javax.swing.border.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+class tablepanel extends JPanel
+{
+	private table[][][] ttable;
+	DefaultTableModel model=new DefaultTableModel();
+	JTable Table;
+	int i;
+	public tablepanel(table[][][] t,int stgrp,inputdata input)
+	{
+		double[] time={8.0,9.0,10.0,11.0,1.0,2.0,3.0}; 
+    		String[] day={"Mon","Tue","Wed","Thurs","Fri"};
+		ttable=t;
+		int stno;
+		Table = new JTable(model){
+			public boolean isCellEditable(int rowIndex, int colIndex) {
+				return false;   //Disallow the editing of any cell
+			}
+		};
+		Table.setRowHeight(30);
+		Table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		//JLabel label=new JLabel(input.stgrp[stno].code);
+		//add(label,BorderLayout.NORTH);
+		model.addColumn("    ");
+		for(i=1;i<=5;i++)
+		{
+			model.addColumn(day[i-1]);
+		}
+		//JScrollBar s=new JScrollBar(HORIZONTAL);
+		for(i=0;i<6;i++)
+		{
+			TableColumn column=Table.getColumnModel().getColumn(i);
+			column.setMinWidth(100);
+   			column.setPreferredWidth(100);
+   		}
+		for(stno=0;stno<stgrp;stno++)
+		{
+			model.addRow(new Object[]{input.stgrp[stno].code,"","","","",""});
+			model.addRow(new Object[]{"","","","","",""});
+			for(i=1;i<=7;i++)
+			{
+				double x=time[i-1]+1;
+				model.addRow(new Object[]{time[i-1]+"-"+x,ttable[stno][i-1][0].course+" "+ttable[stno][i-1][0].ins+" "+ttable[stno][i-1][0].room,ttable[stno][i-1][1].course+" "+ttable[stno][i-1][1].ins+" "+ttable[stno][i-1][1].room,ttable[stno][i-1][2].course+" "+ttable[stno][i-1][2].ins+" "+ttable[stno][i-1][2].room,ttable[stno][i-1][3].course+" "+ttable[stno][i-1][3].ins+" "+ttable[stno][i-1][3].room,ttable[stno][i-1][4].course+" "+ttable[stno][i-1][4].ins+" "+ttable[stno][i-1][4].room});
+				//model.addRow(new Object[]{"","","","","",""});
+			}
+			model.addRow(new Object[]{"","","","","",""});
+			model.addRow(new Object[]{"","","","","",""});
+		}
+		
+		JScrollPane scroll = new JScrollPane(Table);
+scroll.setHorizontalScrollBarPolicy( 
+   JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); 
+scroll.setVerticalScrollBarPolicy( 
+   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		//scroll.getViewport().setExtentSize(new Dimension(100,100));
+		//scroll.setHorizontalScrollBar(s);
+		add(scroll,BorderLayout.CENTER);
+	}
+}
---
+++ src/printtable.java
@@ -0,0 +1,73 @@
+package schedule;
+
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.EventQueue;
+import java.awt.event.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.Graphics2D;
+import javax.swing.border.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+class printtable
+{
+	private table[][][] ttable;
+	JFrame tableframe;
+	int stgrp;
+	int i;
+	tablepanel panel;
+	JPanel south;
+	inputdata input;
+	public printtable(table[][][] t,int nostgrp,inputdata input1)
+	{
+		ttable=t;
+		tableframe=new JFrame();
+		stgrp=nostgrp;
+		south=new JPanel();
+		input=input1;
+	}
+	void print()
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				tableframe.setTitle("CLASS SCHEDULING");
+				tableframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				tableframe.setSize(900,600);
+				tableframe.setLayout(new GridBagLayout());
+				GridBagConstraints c=new GridBagConstraints();
+				c.weightx=0;
+				c.gridx=0;
+				c.gridy=0;
+				c.fill=GridBagConstraints.HORIZONTAL;
+				int j;
+				JButton next=new JButton("EXIT");
+				panel=new tablepanel(ttable,stgrp,input);
+				JPanel south=new JPanel();
+				south.setLayout(new GridLayout(1,10));
+				for(j=0;j<10;j++)
+					south.add(new JLabel(" "));
+				south.add(next);
+				tableframe.add(panel);
+				c.gridx=0;
+				c.gridy=10;
+				tableframe.add(south,c);
+				tableframe.setVisible(true);
+				next.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						
+						System.exit(0);
+					}
+				});
+			}
+		});	
+	}
+}
+
